0a0a0ca5e215f63cd39c41a22c57dbfc25d397be
Richard-tate <62254930+Richard-tate@users.noreply.github.com>
2020-08-21 11:32:18 +0100

    baisc card game

---
+++ slap_jack_game/MOD.js
@@ -0,0 +1,13 @@
+messageOutput_b([opponentCards.length-1]);
+messageOutput( [playerCards.length -1]);
+
+function messageOutputA(mes){
+    // if()
+    message_a.innerHTML = mes;
+}
+function messageOutput_b(mes){
+    message_b.innerHTML = mes;
+}
+function messageOutput(mes){
+    message.innerHTML = mes;
+}
